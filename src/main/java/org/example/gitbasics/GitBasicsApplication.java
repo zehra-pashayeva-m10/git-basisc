1f4fa383ee4532fdd5f045a581e645c0c0beb480
zehra-pashayeva-m10 <zahra.pashayeva@pashapay.az>
2026-02-08 22:41:41 +0400

    initial commit

--- src/main/java/org/example/gitbasics/GitBasicsApplication.java
+++ src/main/java/org/example/gitbasics/GitBasicsApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitBasicsApplication {
+public class GitBasicsApplication {+-
+//    salam
 
     public static void main(String[] args) {
         SpringApplication.run(GitBasicsApplication.class, args);
