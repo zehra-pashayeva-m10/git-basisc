e5d0ea18311d12501aec79ae8fdbe2a3b4a4babe
zehra-pashayeva-m10 <zahra.pashayeva@pashapay.az>
2026-02-08 22:33:24 +0400

    initial commit

---
+++ src/main/java/org/example/gitbasics/GitBasicsApplication.java
@@ -0,0 +1,13 @@
+package org.example.gitbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitBasicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gitbasics/GitBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gitbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
